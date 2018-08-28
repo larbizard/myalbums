484bcc0515146d7a8188347bbf1e2633a5e1087e
Larbi Gharib <larbizard@gmail.com>
2018-08-28 02:29:24 -0400

    Added requirement

--- myalbums/settings.py
+++ myalbums/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
 )
 
 #django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
